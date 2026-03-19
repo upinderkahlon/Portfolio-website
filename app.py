f721519307afd2d397f779149ee2d6ba9f0393d6
Upinder Kahlon <upinderjeetsingh2001@gmail.com>
2026-03-19 20:01:45 +0530

    added newline to app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -11,4 +10,4 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
-app.run(debug=True, host="0.0.0.0", port=80)
+app.run(host="0.0.0.0", port=80)
